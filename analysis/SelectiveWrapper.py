bde07c4d6a53daaa2fb7692a8fbafbd451200241
AlexCawl <babushkinsusu@gmail.com>
2023-12-22 16:22:35 +0500

    feat: made SelectiveWrapper.py

--- analysis/SelectiveWrapper.py
+++ analysis/SelectiveWrapper.py
@@ -1,4 +1,4 @@
-from typing import Optional, Dict, Any, Callable
+from typing import Optional, Dict, Any, Callable, Tuple
 
 import pandas as pd
 
@@ -19,26 +19,58 @@ class SelectiveWrapper(RegressionModelApi):
     # Overall report (used to describe model state after training & testing)
     __report: Dict[str, Any]
 
+    def __init__(self, selector_name: str, model: Callable[[], RegressionModelApi]):
+        self.__selective_variable_name = selector_name
+        self.__factory = model
+        self.__solvers = dict()
+        self.__report = dict()
+        self.__report.update(
+            {
+                "SELECTOR_NAME": self.__selective_variable_name,
+                "SELECTOR_MODEL": self.__factory().__class__.__name__
+            }
+        )
+
     @measure_execution_time
     def get_info(self) -> Dict[Any, Any]:
-        pass
+        for key, model in self.__solvers.items():
+            self.__report.update(
+                {
+                    f"{key}": model.get_info()
+                }
+            )
+        return self.__report
 
     @measure_execution_time
     def train(self, x: pd.DataFrame, y: pd.DataFrame, path: Optional[str] = None) -> None:
-        # get original variables
+        for selector, (x_select, y_select) in self.__get_train_test_selective(x, y).items():
+            model: RegressionModelApi = self.__factory()
+            model.train(x_select, y_select, path)
+            self.__solvers[selector] = model
+
+    @measure_execution_time
+    def test(self, x: pd.DataFrame, y: pd.DataFrame, path: Optional[str] = None) -> None:
+        for selector, (x_select, y_select) in self.__get_train_test_selective(x, y).items():
+            model: RegressionModelApi = self.__solvers[selector]
+            model.test(x_select, y_select, path)
+
+    def __get_train_test_selective(
+            self, x: pd.DataFrame, y: pd.DataFrame
+    ) -> Dict[Any, Tuple[pd.DataFrame, pd.DataFrame]]:
+        # get factors/targets
         factor_vars = list(x.columns.names)
         factor_vars.remove(self.__selective_variable_name)
         target_vars = list(y.columns.names)
 
         # concat to original dataframe
         original_dataframe = pd.concat([x, y], ignore_index=True)
-        selective_variables = set(original_dataframe[self.__selective_variable_name].unique())
 
-        # concat dataframes to original
-        # split it by SELECTOR values
-        # train each model for split df
-        pass
+        # get selective values
+        selective_values = set(original_dataframe[self.__selective_variable_name].unique())
 
-    @measure_execution_time
-    def test(self, x: pd.DataFrame, y: pd.DataFrame, path: Optional[str] = None) -> None:
-        pass
+        # calculate result
+        result: Dict[Any, Tuple[pd.DataFrame, pd.DataFrame]] = dict()
+        for value in selective_values:
+            selected_dataframe = original_dataframe[original_dataframe[self.__selective_variable_name] == value]
+            result[value] = (selected_dataframe[factor_vars], selected_dataframe[target_vars])
+        return result
